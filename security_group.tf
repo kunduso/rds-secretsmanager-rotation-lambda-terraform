32e67d3470c3c4cbde162ba136846dd87e1d6c4c
Sourav Kundu <skundu.dev@gmail.com>
2024-08-24 21:08:54 -0500

    #5 security group for rds

--- security_group.tf
+++ security_group.tf
@@ -2,3 +2,35 @@
 resource "aws_default_security_group" "default" {
   vpc_id = aws_vpc.this.id
 }
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+resource "aws_security_group" "rds" {
+  name        = "${var.name}-rds-sg"
+  description = "Security group for RDS in ${var.name}"
+  vpc_id      = aws_vpc.this.id
+  tags = {
+    "Name" = "${var.name}-sg"
+  }
+  # checkov:skip=CKV2_AWS_5: "Ensure that Security Groups are attached to another resource"
+  # This security group is attached to the Amazon ElastiCache Serverless resource
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
+resource "aws_security_group_rule" "ingress_rds_sg" {
+  description       = "allow traffic to RDS"
+  type              = "ingress"
+  from_port         = 5432
+  to_port           = 5432
+  protocol          = "tcp"
+  cidr_blocks       = [var.vpc_cidr]
+  security_group_id = aws_security_group.rds.id
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
+resource "aws_security_group_rule" "egress_rds_sg" {
+  description       = "allow traffic to reach outside the vpc"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = [var.vpc_cidr]
+  security_group_id = aws_security_group.rds.id
+}
