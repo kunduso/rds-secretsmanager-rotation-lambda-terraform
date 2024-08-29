edfe8e4948fecee9832f16252a1c5053df977870
Sourav Kundu <skundu.dev@gmail.com>
2024-08-29 06:40:27 -0500

    kms key to encrypt #6

--- kms.tf
+++ kms.tf
@@ -10,3 +10,53 @@ resource "aws_kms_alias" "encryption_secret" {
   name          = "alias/${var.name}"
   target_key_id = aws_kms_key.encryption_secret.key_id
 }
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_key
+resource "aws_kms_key" "encryption_rest" {
+  enable_key_rotation     = true
+  description             = "Key to encrypt Amazon CloudWatch logs at rest."
+  deletion_window_in_days = 7
+  #checkov:skip=CKV2_AWS_64: KMS Key policy in a separate resource
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_alias
+resource "aws_kms_alias" "encryption_rest" {
+  name          = "alias/lambda-${var.name}-at-rest"
+  target_key_id = aws_kms_key.encryption_rest.key_id
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_key_policy
+resource "aws_kms_key_policy" "encryption_rest_policy" {
+  key_id = aws_kms_key.encryption_rest.id
+  policy = jsonencode({
+    Id = "encryption-rest"
+    Statement = [
+      {
+        Action = "kms:*"
+        Effect = "Allow"
+        Principal = {
+          AWS = "${local.principal_root_arn}"
+        }
+        Resource = "*"
+        Sid      = "Enable IAM User Permissions"
+      },
+      {
+        Effect : "Allow",
+        Principal : {
+          Service : "${local.principal_logs_arn}"
+        },
+        Action : [
+          "kms:Encrypt*",
+          "kms:Decrypt*",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:Describe*"
+        ],
+        Resource : "*",
+        Condition : {
+          ArnEquals : {
+            "kms:EncryptionContext:aws:logs:arn" : [local.cloudwatch_log_group_arn]
+          }
+        }
+      }
+    ]
+    Version = "2012-10-17"
+  })
+}
