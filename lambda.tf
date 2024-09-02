8cbd864cbc12f4543f6eef6d6375c7f73e400f64
Sourav Kundu <skundu.dev@gmail.com>
2024-09-02 13:02:52 -0500

    #6 updated runtime and the output path

--- lambda.tf
+++ lambda.tf
@@ -2,16 +2,17 @@
 data "archive_file" "python_file" {
   type        = "zip"
   source_dir  = "${path.module}/lambda_function/"
-  output_path = "${path.module}/lambda_function/lambda_function.zip"
+  output_path = "${path.module}/lambda_function.zip"
 }
+
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function
 resource "aws_lambda_function" "secret_rotator" {
-  filename         = "${path.module}/lambda_function/lambda_function.zip"
+  filename         = "${path.module}/lambda_function.zip"
   source_code_hash = data.archive_file.python_file.output_base64sha256
   function_name    = var.name
   role             = aws_iam_role.lambda_role.arn
   handler          = "handler.lambda_handler"
-  runtime          = "python3.12"
+  runtime          = "python3.11"
   kms_key_arn      = aws_kms_key.encryption_rest.arn
   timeout          = 20
   logging_config {
@@ -25,6 +26,7 @@ resource "aws_lambda_function" "secret_rotator" {
       log_stream_name          = aws_cloudwatch_log_stream.log_stream.name
       SECRETS_MANAGER_ENDPOINT = "https://secretsmanager.${var.region}.amazonaws.com"
     }
+
   }
   #checkov:skip=CKV_AWS_50: Not applicable in this use case: X-Ray tracing is enabled for Lambda
   #checkov:skip=CKV_AWS_115: Not applicable in this use case: Ensure that AWS Lambda function is configured for function-level concurrent execution limit
@@ -32,6 +34,9 @@ resource "aws_lambda_function" "secret_rotator" {
   #checkov:skip=CKV_AWS_116: Not applicable in this use case
   #checkov:skip=CKV_AWS_173: Not applicable in this use case
   #checkov:skip=CKV_AWS_272: Not applicable in this use case: Ensure AWS Lambda function is configured to validate code-signing
+  layers = [
+    aws_lambda_layer_version.lambda_layer.arn
+  ]
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_permission
 resource "aws_lambda_permission" "allow_secrets_manager" {
