8cbe59aa1fa1a250c8b25bacb799f8a463d1d5ce
Sourav Kundu <skundu.dev@gmail.com>
2024-09-02 13:01:18 -0500

    added lambda layer for psycopg2

---
+++ lambda_layer.tf
@@ -0,0 +1,12 @@
+#https://registry.terraform.io/providers/hashicorp/archive/latest/docs/data-sources/file
+data "archive_file" "lambda_layer" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda_layer/"
+  output_path = "${path.module}/lambda_layer.zip"
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_layer_version
+resource "aws_lambda_layer_version" "lambda_layer" {
+  filename   = data.archive_file.lambda_layer.output_path
+  layer_name = "postgresql"
+  compatible_runtimes = ["python3.11"]
+}
