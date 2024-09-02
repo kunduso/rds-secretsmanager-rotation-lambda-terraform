f8dc2c72a87aa07d768988edfbaf6600490ced54
Sourav Kundu <skundu.dev@gmail.com>
2024-09-02 12:59:29 -0500

    #6 updated the library

--- lambda_function/handler.py
+++ lambda_function/handler.py
@@ -6,8 +6,7 @@ import boto3
 import json
 import logging
 import os
-import pg
-import pgdb
+import psycopg2
 import time
 
 logger = logging.getLogger()
@@ -17,7 +16,6 @@ def log_cloudwatch(log_group_name,log_stream_name,message):
     # Initialize the Boto3 clients  CloudWatch Logs
     logs_client = boto3.client('logs')
     try:
-
         # Write the parameter value to CloudWatch Logs
         logs_client.put_log_events(
             logGroupName=log_group_name,
@@ -314,7 +312,7 @@ def get_connection(secret_dict):
         secret_dict (dict): The Secret Dictionary
 
     Returns:
-        Connection: The pgdb.Connection object if successful. None otherwise
+        Connection: The psycopg2.Connection object if successful. None otherwise
 
     Raises:
         KeyError: If the secret json does not contain the expected keys
@@ -400,14 +398,14 @@ def connect_and_authenticate(secret_dict, port, dbname, use_ssl):
     try:
         if use_ssl:
             # Setting sslmode='verify-full' will verify the server's certificate and check the server's host name
-            conn = pgdb.connect(host=secret_dict['host'], user=secret_dict['username'], password=secret_dict['password'], database=dbname, port=port,
+            conn = psycopg2.connect(host=secret_dict['host'], user=secret_dict['username'], password=secret_dict['password'], database=dbname, port=port,
                                 connect_timeout=5, sslrootcert='/etc/pki/tls/cert.pem', sslmode='verify-full')
         else:
-            conn = pgdb.connect(host=secret_dict['host'], user=secret_dict['username'], password=secret_dict['password'], database=dbname, port=port,
+            conn = psycopg2.connect(host=secret_dict['host'], user=secret_dict['username'], password=secret_dict['password'], database=dbname, port=port,
                                 connect_timeout=5, sslmode='disable')
         logger.info("Successfully established %s connection as user '%s' with host: '%s'" % ("SSL/TLS" if use_ssl else "non SSL/TLS", secret_dict['username'], secret_dict['host']))
         return conn
-    except pg.InternalError as e:
+    except psycopg2.InternalError as e:
         if "server does not support SSL, but SSL was required" in e.args[0]:
             logger.error("Unable to establish SSL/TLS handshake, SSL/TLS is not enabled on the host: %s" % secret_dict['host'])
         elif re.search('server common name ".+" does not match host name ".+"', e.args[0]):
