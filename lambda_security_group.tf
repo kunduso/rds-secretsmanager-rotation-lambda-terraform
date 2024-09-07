8154c5b8aeb95d4cb3d891089400f76f6d01ccba
Sourav Kundu <skundu.dev@gmail.com>
2024-09-07 07:49:39 -0500

    #13 created endpoint and tightened up security group

--- lambda_security_group.tf
+++ lambda_security_group.tf
@@ -10,53 +10,24 @@ resource "aws_security_group" "lambda" {
   # This security group is attached to the Amazon ElastiCache Serverless resource
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
-resource "aws_security_group_rule" "egress_lambda_sg" {
-  description       = "allow traffic from Lambda"
+resource "aws_security_group_rule" "egress_rds_lambda" {
+  description       = "allow traffic from rds to reach Lambda"
   type              = "egress"
   from_port         = 5432
   to_port           = 5432
   protocol          = "tcp"
-  cidr_blocks       = [var.vpc_cidr]
+  source_security_group_id = aws_security_group.rds.id
+  # cidr_blocks       = [var.vpc_cidr]
   security_group_id = aws_security_group.lambda.id
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
-resource "aws_security_group_rule" "ingress_lambda_sg" {
-  description       = "allow traffic from vpc to the lambda"
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = [var.vpc_cidr]
-  security_group_id = aws_security_group.lambda.id
-}
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
-resource "aws_security_group" "endpoint_sg" {
-  name        = "endpoint_access"
-  description = "allow inbound traffic"
-  vpc_id      = aws_vpc.this.id
-  tags = {
-    "Name" = "${var.name}-endpoint-sg"
-  }
-}
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
-resource "aws_security_group_rule" "ingress_vpc_endpoint" {
-  description       = "Enable access for the endpoints."
-  type              = "ingress"
+resource "aws_security_group_rule" "egress_vpc_endpoint_lambda" {
+  description       = "allow traffic from vpc-endpoint to reach lambda"
+  type              = "egress"
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks       = [aws_vpc.this.cidr_block]
-  security_group_id = aws_security_group.endpoint_sg.id
-}
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_endpoint
-resource "aws_vpc_endpoint" "secrets_manager" {
-  vpc_id              = aws_vpc.this.id
-  service_name        = "com.amazonaws.${var.region}.secretsmanager"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = [for subnet in aws_subnet.db : subnet.id]
-  security_group_ids  = [aws_security_group.endpoint_sg.id]
-  private_dns_enabled = true
-  tags = {
-    "Name" = "${var.name}-secrets-manager"
-  }
+  source_security_group_id = aws_security_group.endpoint_sg.id
+  # cidr_blocks       = [var.vpc_cidr]
+  security_group_id = aws_security_group.lambda.id
 }
