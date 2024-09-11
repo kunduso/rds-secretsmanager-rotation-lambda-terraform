354cbe1136850ca7a07605063ab5482473759507
Sourav Kundu <skundu.dev@gmail.com>
2024-09-11 14:37:36 -0500

    checkov fixes

--- kms.tf
+++ kms.tf
@@ -3,7 +3,6 @@ resource "aws_kms_key" "encryption_rds" {
   enable_key_rotation     = true
   description             = "Key to encrypt secret"
   deletion_window_in_days = 7
-  #checkov:skip=CKV2_AWS_64: Not including a KMS Key policy
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_alias
 resource "aws_kms_alias" "encryption_rds" {
